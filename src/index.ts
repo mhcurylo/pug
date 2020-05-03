d57078a0983cca11d8b3d7b54064fa8fbe67dcb6
Mateusz Curylo <mhcurylo@gmail.com>
2020-05-03 08:25:06 +0100

    animation

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,14 @@ const vertexShaderSource = `#version 300 es
 // an attribute is an input (in) to a vertex shader.
 // It will receive data from a buffer
 in vec2 a_position;
+uniform mediump vec2 u_resolution;
+uniform mediump float u_scroll;
 
 // all shaders have a main function
 void main() {
 
   // convert the position from pixels to 0.0 to 1.0
-  vec2 zeroToOne = a_position / vec2(1300, 800);
+  vec2 zeroToOne = (a_position - vec2(u_scroll, 0)) / u_resolution;
 
   // convert from 0->1 to 0->2
   vec2 zeroToTwo = zeroToOne * 2.0;
@@ -28,15 +30,24 @@ const fragmentShaderSource = `#version 300 es
 // fragment shaders don't have a default precision so we need
 // to pick one. mediump is a good default. It means "medium precision"
 precision mediump float;
+uniform mediump vec2 u_resolution;
+uniform mediump float u_scroll;
 
 // we need to declare an output for the fragment shader
 out vec4 outColor;
 
 void main() {
+  vec2 st = gl_FragCoord.xy / u_resolution;
   // Just set the output to a constant reddish-purple
-  outColor = vec4(1, 0, 0.5, 1);
+  outColor = vec4(pow(u_scroll,0.01), 0, abs(sin(u_scroll*0.02)), 1);
 }
 `
+type Vec2 = { x: number, y: number }
+
+const flatten: (vecs: Vec2[]) => number[]
+  = vecs => vecs.flatMap(({x, y})=> [x, y]);
+
+const resolution: Vec2 = { x: 1300, y: 800 };
 
 const webgl = canvas.getContext("webgl2") as WebGL2RenderingContext
 
@@ -44,7 +55,8 @@ if (!webgl) {
   console.log("NO WEBGL2!")
 }
 
-webgl.viewport(0, 0, canvas.width, canvas.height);
+webgl.viewport(0, 0, resolution.x, resolution.y);
+
 function createShader(gl: WebGL2RenderingContext, type: number, source: string): WebGLShader {
   const shader = gl.createShader(type);
   if (shader) {
@@ -82,13 +94,10 @@ const vertexShader = createShader(webgl, webgl.VERTEX_SHADER, vertexShaderSource
 const fragmentShader = createShader(webgl, webgl.FRAGMENT_SHADER, fragmentShaderSource)
 
 const program = createProgram(webgl, vertexShader, fragmentShader)
-
-
+const resolutionUniformLocation = webgl.getUniformLocation(program, "u_resolution");
+const scrollUniformLocation = webgl.getUniformLocation(program, "u_scroll");
 const positionAttributeLocation = webgl.getAttribLocation(program, "a_position")
 
-
-const height = 800;
-const width = 1300;
 
 const createFloor = (minHeight: number, maxHeight: number, minWidth: number, maxWidth: number, iteration: number) => {
   const floors: number[] = [];
@@ -117,7 +126,11 @@ const positionBuffer = webgl.createBuffer()
 
 webgl.bindBuffer(webgl.ARRAY_BUFFER, positionBuffer);
 
-const positions = createFloor(0, 100, 80, 200, 100).concat(createFloor(800, 700, 80, 200, 100));
+const positions = createFloor(0, 200, 20, 100, 100)
+  .concat(createFloor(800, 600, 20, 40, 100))
+  .concat([0, 0, 0, 50, 1300, 0, 1300, 50])
+  .concat([0, 800, 0, 750, 1300, 800, 1300, 750]);
+
 console.log(positions);
 
 webgl.bufferData(webgl.ARRAY_BUFFER, new Float32Array(positions), webgl.STATIC_DRAW);
@@ -141,11 +154,24 @@ webgl.clear(webgl.COLOR_BUFFER_BIT)
 
 webgl.useProgram(program)
 webgl.bindVertexArray(vao)
+webgl.uniform2f(resolutionUniformLocation, resolution.x, resolution.y);
 
 
-  // draw
-const primitiveType = webgl.TRIANGLE_STRIP;
-webgl.drawArrays(primitiveType, 0, 200);
-webgl.drawArrays(primitiveType, 200, 200);
+const go = (scroll: number) => {
+  webgl.uniform1f(scrollUniformLocation, scroll);
+    // draw
+  const primitiveType = webgl.TRIANGLE_STRIP;
+  webgl.drawArrays(primitiveType, 0, 200);
+  webgl.drawArrays(primitiveType, 200, 200);
+  webgl.drawArrays(primitiveType, 400, 4);
+  webgl.drawArrays(primitiveType, 404, 4);
+
+
+  window.requestAnimationFrame(() => {
+    go(scroll + 1);
+  });
+}
+
+go(0);
 
 console.log('boo')
